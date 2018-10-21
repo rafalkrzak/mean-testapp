5cdb5f7a710b2dd45352289b094bdc621799a22c
krzaku11 <krzak.rafal@gmail.com>
2018-10-21 23:40:30 +0200

    change db ip

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-  database: 'mongodb://68.183.72.152:27017/bucketlist'
+  // database: 'mongodb://68.183.72.152:27017/bucketlist'
+  database: 'mongodb://localhost:27017/bucketlist'
 }
